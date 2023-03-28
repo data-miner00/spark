56b8da868c2122a9c2832281f5cbff225602111f
data-miner00 <data-miner00@users.noreply.github.com>
2023-03-28 18:55:54 +0800

    docs: Added replace empty array sample

---
+++ docs/replace_nested_array.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.types import StructType, StructField, ArrayType, MapType, StringType, IntegerType
+from pyspark.sql.functions import col, when, udf, expr, array, lit, size, coalesce, struct
+
+# define struct with array
+schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("name", StringType(), True),
+    StructField("item", ArrayType(
+        StructType([
+            StructField("mood", StringType(), True),
+            StructField("money", IntegerType(), True)
+        ])
+    ), True)
+])
+
+# create a SparkSession
+spark = SparkSession.builder.appName("sandbox").getOrCreate()
+
+# dummy data with empty array
+data = [
+    ("123", "Kelly", [('happy', 100), ('normal', 20)]),
+    ("124", "Mandy", []),
+]
+
+df = spark.createDataFrame(data=data, schema=schema)
+df.show()
+
+# replace empty array with default value
+df.select(
+    'id',
+    'name',
+    when(
+        size(col("item")) == 0,
+        array(struct(
+            lit("sad").alias("mood"),
+            lit(5).alias("money"))
+        )
+    ).otherwise(col("item")).alias("item")
+).show()
