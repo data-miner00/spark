34e33ef4db117433d9a8ceb90f588529d027d2a4
data-miner00 <data-miner00@users.noreply.github.com>
2023-04-18 17:38:28 +0800

    test: Added test fixture

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from pyspark.sql import SparkSession
+
+
+@pytest.fixture(scope="session")
+def spark(request: pytest.FixtureRequest) -> SparkSession:
+    spark = SparkSession.builder\
+        .master("local")\
+        .getOrCreate()
+    request.addfinalizer(lambda: spark.sparkContext.stop())
+    return spark
